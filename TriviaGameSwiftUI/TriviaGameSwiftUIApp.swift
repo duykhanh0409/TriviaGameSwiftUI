5fed33a6d73b4048b24296b70fbb232d1fad731a
KhanhNguyen <duykhanh.nguyen@unifiedpost.com>
2023-05-08 21:58:18 +0700

    Initial Commit

---
+++ TriviaGameSwiftUI/TriviaGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameSwiftUIApp.swift
+//  TriviaGameSwiftUI
+//
+//  Created by Khanh Nguyen on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
